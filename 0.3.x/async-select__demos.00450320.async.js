e5e431840163eda75d4d2961a0f3585f0c266d48
onlyling <onlylingstudio@gmail.com>
2023-08-08 10:15:46 +0000

    Updates

---
+++ 0.3.x/async-select__demos.00450320.async.js
@@ -0,0 +1,27 @@
+"use strict";(self.webpackChunk_fruits_chain_react_bailu=self.webpackChunk_fruits_chain_react_bailu||[]).push([[8467],{25281:function(P,c,e){e.r(c);var d=e(74077),n=e(67067),E=e(67294),o=e(34169),r=e(49775),a=e(85893),i=[{request:{query:r.I6,variables:{}},result:{data:{demoAsyncSelect:new Array(10).fill(0).map(function(s,u){return{value:u,label:"\u65E0\u53C2\u6570".concat(u)}})}}},{request:{query:r.I6,variables:{page:{pageCurrent:2}}},result:{data:{demoAsyncSelect:new Array(10).fill(0).map(function(s,u){return{value:u,label:"\u6709\u53C2\u6570".concat(u)}})}}}],m=function(){return(0,a.jsx)(d.g,{mocks:i,children:(0,a.jsxs)(o.Card.Group,{children:[(0,a.jsx)(n.Z,{children:(0,a.jsx)(n.Z.Item,{name:"test",label:"\u6D4B\u8BD5",children:(0,a.jsx)(o.AsyncSelect,{remote:{gql:r.I6,gqlKey:"demoAsyncSelect"}})})}),(0,a.jsx)(n.Z,{children:(0,a.jsx)(n.Z.Item,{name:"test",label:"\u643A\u5E26\u8BF7\u6C42\u53C2\u6570",children:(0,a.jsx)(o.AsyncSelect,{remote:{gql:r.I6,gqlKey:"demoAsyncSelect",extraParams:{page:{pageCurrent:2}}}})})})]})})};c.default=m},49775:function(P,c,e){e.d(c,{GQ:function(){return b},Hj:function(){return A},I6:function(){return p},LQ:function(){return D},Ue:function(){return y},jr:function(){return g}});var d=e(97857),n=e.n(d),E=e(68400),o=e.n(E),r=e(17283),a=e(81613),i=e(73359),m,s,u,l={},p=(0,r.Ps)(m||(m=o()([`
+  query demoAsyncSelect($page: Page) {
+    demoAsyncSelect(page: $page) {
+      label
+      value
+    }
+  }
+`])));function b(t){var _=n()(n()({},l),t);return a.a(p,_)}function v(t){var _=_objectSpread(_objectSpread({},l),t);return Apollo.useLazyQuery(p,_)}var D=(0,r.Ps)(s||(s=o()([`
+  query demoPaging1($page: Page, $input: Paging1Input) {
+    demoPaging1(page: $page, input: $input) {
+      records {
+        id
+        name
+      }
+      pageSize
+      pageCurrent
+      totalRecords
+    }
+  }
+`])));function j(t){var _=_objectSpread(_objectSpread({},l),t);return Apollo.useQuery(D,_)}function y(t){var _=n()(n()({},l),t);return i.t(D,_)}var g=(0,r.Ps)(u||(u=o()([`
+  query demoList1($input: Paging1Input) {
+    demoList1(input: $input) {
+      id
+      name
+    }
+  }
+`])));function O(t){var _=_objectSpread(_objectSpread({},l),t);return Apollo.useQuery(g,_)}function A(t){var _=n()(n()({},l),t);return i.t(g,_)}}}]);
---
+++ 0.3.x/hooks__index.md.e38d2a27.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_fruits_chain_react_bailu=self.webpackChunk_fruits_chain_react_bailu||[]).push([[6682],{72892:function(i,u,_){_.r(u);var d=_(71100),r=_(35532),l=_(19532),o=_(10205),s=_(60037),m=_(67915),E=_(66313),t=_(14570),M=_(67294),e=_(85893);function n(){var a=(0,t.eL)(),P=a.texts;return(0,e.jsx)(t.dY,{children:(0,e.jsx)(e.Fragment,{})})}u.default=n}}]);
